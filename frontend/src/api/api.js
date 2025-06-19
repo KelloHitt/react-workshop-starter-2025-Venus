62db8ee12bb45ce6b593ffc22386cbe0a0bdbaa6
KelloHitt <venus.cert@gmail.com>
2025-06-19 12:41:34 +1200

    Completed the retrieveContacts function to return the json format of the response object

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,4 +2,6 @@ const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export async function retrieveContacts() {
     const response = await fetch();
+    const data = await response.json(); // Grabs the json data from response variable
+    return data;
 }
