8d4e6be5172e022cc80c213c92771bf78854d6e3
KelloHitt <venus.cert@gmail.com>
2025-06-20 09:37:38 +1200

    Included BASE_ URL as an argument in fetch function

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const BASE_URL = import.meta.env.VITE_API_BASE_URL; // retrieving the VITE_API_BASE_URL from the .env file
 
 export async function retrieveContacts() {
-    const response = await fetch();
+    const response = await fetch(BASE_URL);
     const data = await response.json(); // Grabs the json data from response variable
     return data;
 }
